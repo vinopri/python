28bb73778ec7d3b86522c50997f0270a14a1382d
Priya Dharshini <145842637+vinopri@users.noreply.github.com>
2025-05-15 11:19:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as str
+# str.title("Hello")
+# str.header("PYTHON")
+# str.subheader("Streamlit")
+str.text("It proposes how social innovation should be recalibrated to meet the increasing threats of the new age of turbulence, including by deploying the sociological lens of the agencystructure dichotomy to show why the public sector needs to become involved more proactively in social innovation. ")
+# str.write("✅❤️‍🔥priya")
+# str.badge("dharshini",color="red")
+# str.code("""
+# #include <stdio.h>
+
+# int main() {
+#   printf("Hello World!");
+#   return 0;
+# }
+# """,language="C")
+# str.divider()
+
+# with str.sidebar:
+#     str.write("this")
+#     str.divider()
+#     str.write("sidebar")
